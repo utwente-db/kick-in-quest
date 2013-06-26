1d15960b8de36ebf7ab59f8ebed5d32f0954517d
retsios <v.retsios@utwente.nl>
2013-06-26 16:16:23 +0200

    First working GPS recording code.

--- assets/www/javascript/quest.js
+++ assets/www/javascript/quest.js
@@ -1,5 +1,7 @@
 // variables
-var gpsTimeoutVal = 60000;
+var gpsTimeoutVal = 6000;
+var watchID = null;
+var fileWriter = null;
 
 // Alias function
 function cl(message) {
@@ -10,31 +12,80 @@ function cl(message) {
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    navigator.geolocation.getCurrentPosition(onGPSSuccess, onGPSError, {enableHighAccuracy: true, timeout: gpsTimeoutVal, maximumAge: 0 });
+	startGPSTracking();
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, openFileSystem, failFS);
+}
+
+function startGPSTracking() {
+    watchID = navigator.geolocation.watchPosition(onGPSSuccess, onGPSError, {enableHighAccuracy: true, timeout: gpsTimeoutVal, maximumAge: 0 });
+}
+
+function endGPSTracking() {
+	navigator.geolocation.clearWatch(watchID);
 }
 
 /**
  * onGPSSuccess receives a Position object
  */
 function onGPSSuccess(position) {
-//	var message = 'Latitude: '             + position.coords.latitude              + '<br />' +
-//                  'Longitude: '          + position.coords.longitude             + '<br />' +
-//                  'Altitude: '           + position.coords.altitude              + '<br />' +
-//                  'Accuracy: '           + position.coords.accuracy              + '<br />' +
-//                  'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-//                  'Heading: '            + position.coords.heading               + '<br />' +
-//                  'Speed: '              + position.coords.speed                 + '<br />' +
-//                  'Timestamp: '          + position.timestamp          			 + '<br />';
-    var message = 'Position at ' + position.timestamp + ':\n' + 
-    			  position.coords.latitude + ', ' + position.coords.longitude;
-    
-	alert(message);
+	if (fileWriter != null) {
+		message = "" + position.coords.latitude + "," + position.coords.longitude + "," + position.coords.altitude + "," + position.coords.accuracy
+		+ "," + position.coords.altitudeAccuracy + "," + position.coords.heading + "," + position.coords.speed + "," + position.timestamp + "\n";          		
+		fileWriter.write(message);
+	} else
+		alert("Readonly SD-card;\nPosition at " + position.timestamp + ":\n" + position.coords.latitude + ", " + position.coords.longitude);
 }
 
 /**
  * onGPSError Callback receives a PositionError object
  */
 function onGPSError(error) {
-	alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+	alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
 }
+
+function openFileSystem(fileSystem) {
+	fileSystem.root.getDirectory("KickInQuest", {create: true, exclusive: false}, onGetDirectorySuccess, onGetDirectoryFail);
+}
+
+function onGetDirectorySuccess(directory) {
+	directory.getFile("gpsdata.txt", {create: true, exclusive: false}, createFile, failFE);
+}
+
+function createFile(fileEntry) {
+	fileEntry.createWriter(createFileWriter, failW);
+}
+
+function createFileWriter(writer) {
+	/*
+    writer.onwriteend = function(evt) {
+    	alert("success writing file");
+    };
+    */
+	fileWriter = writer;
+	fileWriter.write("Latitude,Longitude,Altitude,Accuracy,Altitude Accuracy,Heading,Speed,Timestamp\n");
+}
+
+function failFS(evt) {
+	alert("Opening file System failed with error " + evt.code);
+	//alert(evt.code);
+	//alert(evt.message);
+	//alert(evt.target.error.code);
+	//console.log(evt.target.error.code);
+}
+
+function failFE(evt) {
+	alert("Opening file for writing failed with error " + evt.code);
+	// alert(evt.target.error.code);
+	//console.log(evt.target.error.code);
+}
+
+function failW(evt) {
+	alert("Creating FileWriter failed with error " + evt.code);
+	//alert(evt.target.error.code);
+	//console.log(evt.target.error.code);
+}
+
+function onGetDirectoryFail(evt) {
+	alert("Creating directory failed with error " + evt.code);
+}
+
