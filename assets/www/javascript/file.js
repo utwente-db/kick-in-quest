f5d8fdda8f87336cbcd8cd190409d64dff52faa1
retsios <v.retsios@utwente.nl>
2013-06-28 12:29:38 +0200

    KIQ-2 eerste code uploaden coordinaten

--- assets/www/javascript/file.js
+++ assets/www/javascript/file.js
@@ -1,3 +1,5 @@
+// Store GPS data to SD-Card
+
 var fileWriter = null;
 
 function openFileSystem(fileSystem) {
@@ -46,6 +48,33 @@ function onGetDirectoryFail(evt) {
 	alert("Creating directory failed with error " + evt.code);
 }
 
+// Upload GPS data to Server using HTTP POST
+// TODO: test this on a real server!
+// TODO: check if we want to automatically have sourceFileURI be something like /sdcard/appSDFolder/gpsFileName
+
+function uploadFile(sourceFileURI, serverURI) {
+    var options = new FileUploadOptions();
+    options.fileKey="file";
+    options.fileName=sourceFileURI.substr(sourceFileURI.lastIndexOf('/') + 1); // only the filename
+    options.mimeType="text/plain";
+    var params = {};
+    options.params = params;
+    var ft = new FileTransfer();
+    ft.upload(sourceFileURI, encodeURI(serverURI), onUploadOK, onUploadFail, options);	
+}
+
+function onUploadOK(metadata) {
+	alert("Uploaded " + (Math.round(metadata.bytesSent / 102.4) / 10) + " MB");
+	console.log("Code = " + metadata.responseCode);
+	console.log("Response = " + metadata.response);
+	console.log("Sent = " + metadata.bytesSent);
+}
+
+function onUploadFail(error) {
+	alert("An error has occurred: Code = " + error.code);
+	console.log("upload error source " + error.source);
+	console.log("upload error target " + error.target);
+}
 
 /*
  * DOWNLOAD FILE
